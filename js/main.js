3bb1ad50c9fddbde3ab62c48a54129aa3898e28e
emj <eduard.maju@gmail.com>
2025-04-10 13:02:29 -0600

    Se completan los objetivos del 1 al 5

--- js/main.js
+++ js/main.js
@@ -1,18 +1,102 @@
-let txtName = document.getElementById("Name");
-let txtNumber = document.getElementById("Number");
-let btnAgregar = document.getElementById("btnAgregar");
-let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
-let alertValidaciones = document.getElementById("alertValidaciones");
+const txtName = document.getElementById("Name"); // Nombre
+const txtNumber = document.getElementById("Number"); // Cantidad
+const btnAgregar = document.getElementById("btnAgregar");
+const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const alertValidaciones = document.getElementById("alertValidaciones");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+
+const contadorProductos = document.getElementById("contadorProductos");
+const productosTotal = document.getElementById("productosTotal");
+const precioTotal = document.getElementById("precioTotal");
+
+// Contador para la primera columna de la tabla
+let cont = 0;
+// Contador para la columna de resumen
+let costoTotal = 0;
+let totalEnProductos = 0;
+
+function validarCantidad(){
+    // length < 0 - Valida que no este vacio 
+    if (txtNumber.value.trim().length <= 0) { 
+        return false;
+    } 
+
+    // Valida que es un valor numerico 
+    // isNAN = true - No es un número
+    // isNAN = false - Es un número 
+    // Se valida antes del if <= 0 porque no tendria caso hacer una valiación con un dato no numerico
+    if (isNaN(txtNumber.value)) {
+        return false;
+    }
+    
+    // Valida no tener cantidades negativas (<=0), por logica del ejercicio 
+    if (Number(txtNumber.value) <= 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Crear precios al azar
+// Math.random() - Genera números entre 0 y 1
+// Math.round() - Redondea al entero más proximo sea hacia arriba o abajo
+
+function getPrecio() {
+    return Math.round(Math.random()*10000) / 100;
+}
+
 
 btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
 
-    txtName.value = txtName.value.trim();
-    txtNumber.value = txtNumber.value.trim();
+    // Bandera, al ser true permite agregar los datos a la tabla
+    let isValid = true;
 
-    if (txtName.value.length < 3) {
-        txtName.style.border = "solid medium red";
-        alertValidacionesTexto.innerHTML = "<strong>El Nombre del producto no es correcto.</strong>"
+    alertValidacionesTexto.innerHTML = ""; // Evita que aparezca el mensaje de alerta si el producto es valido
+    alertValidaciones.style.display = "none"; // Evita que aparezca el bloque de alerta si el producto es valido
+    txtName.style.border = ""; // Evita los estilos en el bloque si el producto es valido
+    txtNumber.style.border = "";
+
+    txtName.value = txtName.value.trim(); // Quita los espacios en blanco antes del texto
+    txtNumber.value = txtNumber.value.trim(); // Quita los espacios en blanco antes del texto
+
+    if (txtName.value.length < 3) { // Si es menor a 3, mostrara una alerta de producto invalido
+        txtName.style.border = "solid medium red"; // Estilo del bloque de alerta
+        alertValidacionesTexto.innerHTML = "<strong>El Nombre del producto no es correcto.</strong>"; // Mensaje de alerta
+        alertValidaciones.style.display = "block"; // El bloque de alerta se muestra en bloque
+    }
+
+    if (! validarCantidad()) { 
+        txtNumber.style.border = "solid medium red"; 
+        alertValidacionesTexto.innerHTML += "<br/><strong>La cantidad no es correcta.</strong>"; 
         alertValidaciones.style.display = "block";
     }
-});
+
+    // Si paso las validaciones
+    if (isValid) {
+        cont++;
+        let precio = getPrecio(); // Ultima columna
+        let row = ` <tr>
+                        <td>${cont}</td>
+                        <td>${txtName.value}</td>
+                        <td>${txtNumber.value}</td>
+                        <td>${precio}</td>
+                    </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        // Total de cantidad de productos 
+        costoTotal += precio * Number(txtNumber.value);
+        precioTotal.innerText = "$" + costoTotal.toFixed(2);
+        // Precio total
+        totalEnProductos += Number(txtNumber.value);
+        productosTotal.innerText = totalEnProductos;
+        // Conteo total de diferentes productos
+        contadorProductos.innerText = cont; // Se reutiliza el contador de columna
+
+        txtName.value = "";
+        txtNumber.value = "";
+        txtName.focus();
+    } // if isValid
+
+
+}); // btnAgregar
